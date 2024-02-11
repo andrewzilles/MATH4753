4f2309bff3215f47f34cec02a31ad7cc4f196612
Andrew <andrew.zilles@ou.edu>
2024-02-11 01:07:57 -0600

    created a myquad function and aparently a myplot function that hasn't been committed

--- R/myquad.R
+++ R/myquad.R
@@ -1,12 +1,19 @@
 #' Quadratic function
 #'
 #' @param x numeric vector
+#' @param lm_model linear regression model object
 #'
-#' @return numeric vector
+#'
+#'
+#' @return A quadratic equation
 #' @export
 #'
+#'
 #' @examples
-#' myquad(x = 1:10)
-myquad <- function(x){
-  x^2 -5*x +6
+#' \dontrun{myquad(x = 1:10, df.lm)}
+#'
+#'
+myquad <- function(x,lm_model){
+  coef <- coef(lm_model) #Extract coefficients from the linear model
+coef[1] + coef[2] * x + coef[3] * x^2
 }
